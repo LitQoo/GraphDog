4f98d3165bb243199f50c23fac4fe59f8e823e7d
seo88youngho <seo88youngho@gmail.com>
2013-05-08 14:34:11 +0900

    cocos version

--- GDLib.cpp
+++ GDLib.cpp
@@ -9,6 +9,7 @@
 #include "android/jni/JniHelper.h"
 #include "android/jni/SystemInfoJni.h"
 #else
+#include "platform/android/jni/Java_org_cocos2dx_lib_Cocos2dxHelper.h"
 #include "platform/android/jni/JniHelper.h"
 #endif
 #endif
